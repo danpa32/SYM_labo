602492cf935345b984ffb1b0f939eaf74775e0ee
Guillaume Milani <milani.guillaume@gmail.com>
2017-12-15 10:12:25 +0100

    First working compass

---
+++ labo3/SYM_labo3/app/src/main/java/ch/heigvd/iict/sym/sym_labo3/a3dcompassapp/CompassActivity.java
@@ -0,0 +1,31 @@
+package ch.heigvd.iict.sym.sym_labo3.a3dcompassapp;
+
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import ch.heigvd.iict.sym.sym_labo3.R;
+
+public class CompassActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+    /* TODO */
+    // your activity need to register accelerometer and magnetometer sensors' updates
+    // then you may want to call
+    //  this.opglr.swapRotMatrix()
+    // with the 4x4 rotation matrix, everytime a new matrix is computed
+    // more information on rotation matrix can be found on-line:
+    // https://developer.android.com/reference/android/hardware/SensorManager.html#getRotationMatrix(float[],%20float[],%20float[],%20float[])
+
+}
